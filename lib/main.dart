50f6f10a1a8bfb61c87a907e8d6056a6c905e31e
Mehedi <babu.oshi@gmail.com>
2021-03-23 19:44:27 +0600

    Complete codes with updated Widgets.

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello World Travel Title",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World Travel App"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Builder(
+          builder: (context) => Center(
+            child: Column(
+              children: [
+                Text(
+                  "Hello World Travel",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[800],
+                  ),
+                ),
+                Text(
+                  "Discover the World",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                ),
+                Image.network(
+                  'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+                  height: 350,
+                ),
+                ElevatedButton(
+                  onPressed: () => contactUS(context),
+                  child: Text('Contact us'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void contactUS(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Contact Us"),
+          content: Text("Mail us at hello@world.com"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Close"),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
