0d50995e1738942decb77d2d023e7095b2efba46
Mehedi <babu.oshi@gmail.com>
2021-03-24 14:42:53 +0600

    padding and SingleChildscrollView added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,33 +14,50 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepPurple,
         ),
         body: Builder(
-          builder: (context) => Center(
-            child: Column(
-              children: [
-                Text(
-                  "Hello World Travel",
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue[800],
-                  ),
+          builder: (context) => SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "Hello World Travel",
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[800],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        "Discover the World",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.deepPurpleAccent,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Image.network(
+                        'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+                        height: 350,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: ElevatedButton(
+                        onPressed: () => contactUS(context),
+                        child: Text("Contact Us"),
+                      ),
+                    )
+                  ],
                 ),
-                Text(
-                  "Discover the World",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                ),
-                Image.network(
-                  'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
-                  height: 350,
-                ),
-                ElevatedButton(
-                  onPressed: () => contactUS(context),
-                  child: Text('Contact us'),
-                )
-              ],
+              ),
             ),
           ),
         ),
